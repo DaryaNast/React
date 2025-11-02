b380ae782ef705373d79ec4d5fe8cbcb21f7e726
Darya <nastenko_1999@mail.ru>
2025-11-02 14:00:33 +0700

    1.3.6

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/your-repo-name/',
+  build: {
+    outDir: 'docs'
+  },
   test: {
     globals: true,
     environment: 'jsdom',
